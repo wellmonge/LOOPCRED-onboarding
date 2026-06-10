338cfbd70baebe321fe4198e856bfb97a4ed9cc2
Wellinton Monge <mongewellinton@gmail.com>
2026-06-10 18:33:56 -0400

    initial

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'assets',
+  base: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/',
 })
